417cff720ed2cede01c94933a55ea5c3f14a6793
gracypaul <88486907+gracypaul@users.noreply.github.com>
2021-11-29 00:34:04 +0530

    Update MainActivity1.java

--- app/src/main/java/com/example/login/MainActivity1.java
+++ app/src/main/java/com/example/login/MainActivity1.java
@@ -21,7 +21,7 @@ public class MainActivity1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.create_account);
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         phone = findViewById(R.id.phone);
