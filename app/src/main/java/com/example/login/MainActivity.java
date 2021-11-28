7a3074a10a8ed071f45365ea485440fb1a804109
gracypaul <88486907+gracypaul@users.noreply.github.com>
2021-11-29 00:38:42 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -2,13 +2,97 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.wifi.hotspot2.pps.Credential;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText username;
+    EditText password;
+    Button signup;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupUI();
+        setupListeners();
+    }
+    private void setupUI() {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signup = findViewById(R.id.createaccount);
+        login = findViewById(R.id.loginbtn);
+    }
+    private void setupListeners() {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUsername();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
+                //startActivity(i);
+            }
+        });
+    }
+    void checkUsername() {
+        boolean isValid = true;
+        if (isEmpty(username)) {
+            username.setError("You must enter username to login!");
+            isValid = false;
+        } else {
+            if (!isEmail(username)) {
+                username.setError("Enter valid email!");
+                isValid = false;
+            }
+        }
+
+        if (isEmpty(password)) {
+            password.setError("You must enter password to login!");
+            isValid = false;
+        } else {
+            if (password.getText().toString().length() < 4) {
+                password.setError("Password must be at least 4 chars long!");
+                isValid = false;
+            }
+        }
+
+        if (isValid) {
+            String usernameValue = username.getText().toString();
+            String passwordValue = password.getText().toString();
+            if (usernameValue.equals("test@test.com") && passwordValue.equals("password1234")) {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(i);
+                this.finish();
+            } else {
+                Toast t = Toast.makeText(this, "Wrong email or password!", Toast.LENGTH_SHORT);
+                t.show();
+            }
+        }
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
     }
 }
